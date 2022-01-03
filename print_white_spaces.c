f82493eae8845fc4fe5f177d5c1dfa68c3a45fe0
panditprogrammer <panditprogrammer@gmail.com>
2022-01-03 13:27:27 +0530

    print white spaces

---
+++ print_white_spaces.c
@@ -0,0 +1,15 @@
+//printf function and \ character
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+
+    system("cls");
+    printf("programming is fun.\
+           this is not true.");  //backslash is taking white spaces as string
+
+    printf("\nprogramming is fun."
+           "this is again not true.");
+    system("pause");
+}
